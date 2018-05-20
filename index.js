40e7042e9fef417a3df1712f34a6ec1370b879ff
Blanca Hernandez <blamca1029@gmail.com>
2018-05-19 20:38:29 -0700

    finalized changes to Making-App Accessible

--- index.js
+++ index.js
@@ -1,54 +1,52 @@
 $(document).ready(function () {
 
     $("#js-search-form").submit(function (event) {
-       
+
         event.preventDefault();
-        
+
         var userInput = $("#js-query").val();
-   
+
         getResults(userInput);
     });
 
-    
+
     function getResults(userSearchTerm) {
         $.getJSON("https://www.googleapis.com/youtube/v3/search", {
-                part: "snippet", 
-                maxResults: 20, 
+                part: "snippet",
+                maxResults: 20,
                 key: "AIzaSyCl-Pw8hO-CdGkhwG8Js7Lbwp3B-yTMAnc",
-                q: userSearchTerm, 
-                type: "video" 
+                q: userSearchTerm,
+                type: "video"
             },
             function (receivedApiData) {
-                
+
                 console.log(receivedApiData);
-                
+
                 if (receivedApiData.pageInfo.totalResults == 0) {
                     alert("No videos found!");
-                }
-                
-                else {
+                } else {
                     displaySearchResults(receivedApiData.items);
                 }
             });
     }
 
-  
+
     function displaySearchResults(videosArray) {
 
-      
+
         var buildTheHtmlOutput = "";
 
         $.each(videosArray, function (videosArrayKey, videosArrayValue) {
-            
+
             buildTheHtmlOutput += "<li>";
-            buildTheHtmlOutput += "<p>" + videosArrayValue.snippet.title + "</p>"; 
-            buildTheHtmlOutput += "<a href='https://www.youtube.com/watch?v=" + videosArrayValue.id.videoId + "' target='_blank'>"; 
-            buildTheHtmlOutput += "<img src='" + videosArrayValue.snippet.thumbnails.high.url + "'/>"; 
+            buildTheHtmlOutput += "<p>" + videosArrayValue.snippet.title + "</p>";
+            buildTheHtmlOutput += "<a aria-label='" + videosArrayValue.snippet.title + "' href='https://www.youtube.com/watch?v=" + videosArrayValue.id.videoId + "' target='_blank'>";
+            buildTheHtmlOutput += "<img src='" + videosArrayValue.snippet.thumbnails.high.url + "'/>";
             buildTheHtmlOutput += "</a>";
             buildTheHtmlOutput += "</li>";
         });
 
-        
+
         $("#js-search-results ul").html(buildTheHtmlOutput);
     }
 });
